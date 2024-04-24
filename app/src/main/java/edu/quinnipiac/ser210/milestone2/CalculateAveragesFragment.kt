3be08c6dbded8f94d1768db79f497abc2fa68a30
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2024-04-24 13:14:37 -0400

    Added ability to customize promotion level

--- app/src/main/java/edu/quinnipiac/ser210/milestone2/CalculateAveragesFragment.kt
+++ app/src/main/java/edu/quinnipiac/ser210/milestone2/CalculateAveragesFragment.kt
@@ -1,38 +1,44 @@
 package edu.quinnipiac.ser210.milestone2
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.asLiveData
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import edu.quinnipiac.ser210.milestone2.databinding.FragmentCalculateAveragesBinding
 import kotlinx.coroutines.launch
+import kotlin.math.max
 
 
 class CalculateAveragesFragment : Fragment() {
-    private lateinit var binding: FragmentCalculateAveragesBinding
+	private lateinit var binding: FragmentCalculateAveragesBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentCalculateAveragesBinding.inflate(layoutInflater)
-        update()
-        binding.updateButton.setOnClickListener {
-            update()
-        }
-        return binding.root
-    }
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		binding = FragmentCalculateAveragesBinding.inflate(layoutInflater)
+		update()
+		binding.updateButton.setOnClickListener {
+			update()
+		}
+		return binding.root
+	}
 
-    private fun update() {
-        val recyclerView = binding.recyclerView
-        val characterDao = (activity?.application as DataApplication).characterDatabase.characterDao()
-        lifecycleScope.launch {
-            characterDao.getCharacter(binding.characterNameEditText.text.toString()).collect {
-                recyclerView.adapter = AveragesAdapter(it, 20)
-            }
-        }
-    }
+	private fun update() {
+		val recyclerView = binding.recyclerView
+		val characterDao =
+			(activity?.application as DataApplication).characterDatabase.characterDao()
+		val promotionLevel: Int = binding.promotionLevelEditText.text.toString().toInt()
+		lifecycleScope.launch {
+			characterDao.getCharacter(binding.characterNameEditText.text.toString()).collect {
+				if (promotionLevel <= 20 && (promotionLevel >= max(10, it.baseLevel))) {
+					recyclerView.adapter =
+						AveragesAdapter(it, if (it.canPromote) promotionLevel else 20)
+					binding.promotionLevelEditText.isEnabled = it.canPromote
+				}
+			}
+		}
+	}
 }
