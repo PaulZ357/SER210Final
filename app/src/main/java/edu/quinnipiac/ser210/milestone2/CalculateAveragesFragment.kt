528b6707b5ea9b95bd6dbeada821c866bd1177da
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2024-04-24 12:42:50 -0400

    Added ability to change character in average calaculator

--- app/src/main/java/edu/quinnipiac/ser210/milestone2/CalculateAveragesFragment.kt
+++ app/src/main/java/edu/quinnipiac/ser210/milestone2/CalculateAveragesFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
 import edu.quinnipiac.ser210.milestone2.databinding.FragmentCalculateAveragesBinding
+import kotlinx.coroutines.launch
 
 
 class CalculateAveragesFragment : Fragment() {
@@ -16,9 +19,20 @@ class CalculateAveragesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentCalculateAveragesBinding.inflate(layoutInflater)
-        val recyclerView = binding.recyclerView
-        val leif = (activity?.application as DataApplication).defaultCharacters[0]
-        recyclerView.adapter = AveragesAdapter(leif, 20)
+        update()
+        binding.updateButton.setOnClickListener {
+            update()
+        }
         return binding.root
+    }
+
+    private fun update() {
+        val recyclerView = binding.recyclerView
+        val characterDao = (activity?.application as DataApplication).characterDatabase.characterDao()
+        lifecycleScope.launch {
+            characterDao.getCharacter(binding.characterNameEditText.text.toString()).collect {
+                recyclerView.adapter = AveragesAdapter(it, 20)
+            }
+        }
     }
 }
