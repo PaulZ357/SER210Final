def9a291561c027fa3422bb6abe7a337c7ea22f4
PaulZ357 <Paul.Zegarek@quinnipiac.edu>
2024-04-24 12:56:30 -0400

    add scroll ok button listeners

--- app/src/main/java/edu/quinnipiac/ser210/milestone2/AddCharacterFragment.kt
+++ app/src/main/java/edu/quinnipiac/ser210/milestone2/AddCharacterFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import edu.quinnipiac.ser210.milestone2.databinding.FragmentAddCharacterBinding
+import edu.quinnipiac.ser210.milestone2.databinding.FragmentEditCharacterBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,12 +22,16 @@ class AddCharacterFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
+    lateinit var binding: FragmentAddCharacterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+        }
+        binding = FragmentAddCharacterBinding.inflate(layoutInflater)
+        binding.addCharacterOkButton.setOnClickListener {
+            // ok button
         }
     }
 
