1d811635e0c3331665d64c0a957cbf88d41570dd
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2024-04-27 19:40:01 -0400

    Refined average calculations

--- app/src/main/java/edu/quinnipiac/ser210/milestone2/CalculateAveragesFragment.kt
+++ app/src/main/java/edu/quinnipiac/ser210/milestone2/CalculateAveragesFragment.kt
@@ -4,70 +4,74 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Adapter
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemSelectedListener
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.asLiveData
-import androidx.lifecycle.lifecycleScope
+import androidx.fragment.app.activityViewModels
 import edu.quinnipiac.ser210.milestone2.data.Character
-import edu.quinnipiac.ser210.milestone2.data.CharacterDao
 import edu.quinnipiac.ser210.milestone2.databinding.FragmentCalculateAveragesBinding
-import kotlinx.coroutines.launch
 
 
 class CalculateAveragesFragment : Fragment() {
 	private lateinit var binding: FragmentCalculateAveragesBinding
-	private lateinit var characterDao: CharacterDao
+
+	private val viewModel: CalculateAveragesViewModel by activityViewModels {
+		CalculateAveragesViewModelFactory((activity?.application as DataApplication).characterDatabase.characterDao())
+	}
 
 	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View {
 		binding = FragmentCalculateAveragesBinding.inflate(layoutInflater)
-
-		characterDao = (activity?.application as DataApplication).characterDatabase.characterDao()
-		characterDao.getCharacters().asLiveData()
-			.observe(viewLifecycleOwner) { characters: List<Character> ->
-				binding.characterNameSpinner.adapter =
-					ArrayAdapter(
-						requireContext(),
-						android.R.layout.simple_spinner_item,
-						Array(characters.size) { characters[it].name })
-				val character = characters[0]
-				binding.promotionLevelSpinner.adapter = ArrayAdapter(
+		viewModel.characters.observe(viewLifecycleOwner) { characters: List<Character> ->
+			binding.characterNameSpinner.adapter =
+				ArrayAdapter(
 					requireContext(),
 					android.R.layout.simple_spinner_item,
-					arrayOf(if (character.baseLevel > 10) character.baseLevel else 10)
-				)
-				update()
-			}
+					Array(characters.size) { characters[it].name })
+			binding.characterNameSpinner.setSelection(viewModel.characterIndex.value!!)
+		}
+		viewModel.adapter.observe(viewLifecycleOwner) {
+			binding.recyclerView.adapter = it
+		}
 
-		binding.updateButton.setOnClickListener { update() }
+		viewModel.levels.observe(viewLifecycleOwner) {
+			binding.promotionLevelSpinner.adapter =
+				ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, it)
+			binding.promotionLevelSpinner.setSelection(viewModel.levelIndex.value!!)
+		}
+
+		binding.characterNameSpinner.onItemSelectedListener = CharacterListener(viewModel)
+		binding.promotionLevelSpinner.onItemSelectedListener = LevelListener(viewModel)
 
 		return binding.root
 	}
 
-	private fun update() {
-		val recyclerView = binding.recyclerView
-		val promotionLevel = binding.promotionLevelSpinner.selectedItem as Int
-		lifecycleScope.launch {
-			characterDao.getCharacter(binding.characterNameSpinner.selectedItem as String)
-				.collect { character: Character ->
-					recyclerView.adapter =
-						AveragesAdapter(character, if (character.canPromote) promotionLevel else 20)
-					val startingLevel = if (character.baseLevel > 10) character.baseLevel else 10
-					if (character.canPromote) {
-						val levels = Array(21 - startingLevel) { it + 10 }
-						binding.promotionLevelSpinner.adapter = ArrayAdapter(
-							requireContext(),
-							android.R.layout.simple_spinner_item,
-							levels
-						)
-					}
-					binding.promotionLevelSpinner.isEnabled = character.canPromote
-				}
+	class CharacterListener(private val viewModel: CalculateAveragesViewModel) :
+		OnItemSelectedListener {
+		override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+			viewModel.setCharacterIndex(position)
+		}
+
+		override fun onNothingSelected(parent: AdapterView<*>?) {
+		}
+
+	}
+
+	class LevelListener(private val viewModel: CalculateAveragesViewModel) :
+		OnItemSelectedListener {
+		override fun onItemSelected(
+			parent: AdapterView<*>?,
+			view: View?,
+			position: Int,
+			id: Long
+		) {
+			viewModel.setLevelIndex(position)
+		}
+
+		override fun onNothingSelected(parent: AdapterView<*>?) {
 		}
 	}
 }
---
+++ app/src/main/java/edu/quinnipiac/ser210/milestone2/CalculateAveragesViewModel.kt
@@ -0,0 +1,93 @@
+package edu.quinnipiac.ser210.milestone2
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import edu.quinnipiac.ser210.milestone2.data.Character
+import edu.quinnipiac.ser210.milestone2.data.CharacterDao
+import kotlinx.coroutines.launch
+import kotlin.math.max
+
+class CalculateAveragesViewModel(private val characterDao: CharacterDao) : ViewModel() {
+	val characters: LiveData<List<Character>> = characterDao.getCharacters().asLiveData()
+	val characterIndex: LiveData<Int>
+		get() = _characterIndex
+	private var _characterIndex = MutableLiveData(0)
+	val levelIndex: LiveData<Int>
+		get() = _levelIndex
+	private var _levelIndex = MutableLiveData(10)
+	val levels: LiveData<Array<Int>>
+		get() = _levels
+	private var _levels = MutableLiveData(Array(11) { it + 10 })
+	val adapter: LiveData<AveragesAdapter>
+		get() = _adapter
+	private var _adapter = MutableLiveData<AveragesAdapter>()
+
+	init {
+		updateLevels()
+		updateAdapter()
+	}
+
+	fun setCharacter(index: Int) {
+		if (_characterIndex.value != index) {
+			_characterIndex.value = index
+			updateLevels()
+			updateAdapter()
+		}
+	}
+
+	private fun updateLevels() {
+		viewModelScope.launch {
+			characterDao.getCharacters().collect { characterList ->
+				val character = characterList[characterIndex.value!!]
+				val baseLevel: Int = character.baseLevel
+				if (baseLevel != (levels.value?.get(0) ?: 10)) {
+					val promotionLevel = max(baseLevel, 10)
+					val levelsArray = Array(21 - promotionLevel) { it + promotionLevel }
+					_levels = MutableLiveData(levelsArray)
+					if ((levelIndex.value!! >= levelsArray.size)) {
+						_levelIndex.value = levelsArray.size - 1
+					}
+				}
+
+			}
+		}
+	}
+
+	private fun updateAdapter() {
+		viewModelScope.launch {
+			characterDao.getCharacters().collect {
+				val character = it[characterIndex.value!!]
+				val promotionLevel = levels.value?.get(levelIndex.value!!)
+				_adapter.value = AveragesAdapter(
+					character,
+					if ((promotionLevel != null) && character.canPromote) promotionLevel else 20
+				)
+			}
+		}
+	}
+
+	fun setCharacterIndex(index: Int) {
+		_characterIndex.value = index
+		updateAdapter()
+	}
+
+	fun setLevelIndex(index: Int) {
+		_levelIndex.value = index
+		updateAdapter()
+	}
+}
+
+class CalculateAveragesViewModelFactory(private val characterDao: CharacterDao) :
+	ViewModelProvider.Factory {
+	override fun <T : ViewModel> create(modelClass: Class<T>): T {
+		if (modelClass.isAssignableFrom(CalculateAveragesViewModel::class.java)) {
+			@Suppress("UNCHECKED_CAST")
+			return CalculateAveragesViewModel(characterDao) as T
+		}
+		throw IllegalArgumentException("Unknown ViewModel class")
+	}
+}
