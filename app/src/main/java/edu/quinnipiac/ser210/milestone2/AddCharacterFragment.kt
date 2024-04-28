5fe6e2a4eedf6fa53cab12b8c27152717e69b3b0
PaulZ357 <Paul.Zegarek@quinnipiac.edu>
2024-04-27 21:51:42 -0400

    UI fix for AddCharacterFragment

--- app/src/main/java/edu/quinnipiac/ser210/milestone2/AddCharacterFragment.kt
+++ app/src/main/java/edu/quinnipiac/ser210/milestone2/AddCharacterFragment.kt
@@ -39,7 +39,28 @@ class AddCharacterFragment : Fragment() {
             Toast.makeText(this.context,"Added character",Toast.LENGTH_SHORT).show()
             val characterDao =
                 (activity?.application as DataApplication).characterDatabase.characterDao()
-            val char = Character(id,name.toString(),0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,true)
+            val char = Character(id,
+                name.toString(),
+                binding.addCharacterBaseLevel.text.toString().toInt(),
+                binding.addCharacterBaseHP.text.toString().toInt(),
+                binding.addCharacterBaseStr.text.toString().toInt(),
+                binding.addCharacterBaseMag.text.toString().toInt(),
+                binding.addCharacterBaseSkill.text.toString().toInt(),
+                binding.addCharacterBaseSpeed.text.toString().toInt(),
+                binding.addCharacterBaseLuck.text.toString().toInt(),
+                binding.addCharacterBaseDef.text.toString().toInt(),
+                binding.addCharacterBaseCon.text.toString().toInt(),
+                binding.addCharacterBaseMove.text.toString().toInt(),
+                binding.addCharacterHPGrowth.text.toString().toInt(),
+                binding.addCharacterStrGrowth.text.toString().toInt(),
+                binding.addCharacterMagGrowth.text.toString().toInt(),
+                binding.addCharacterSkillGrowth.text.toString().toInt(),
+                binding.addCharacterSpeedGrowth.text.toString().toInt(),
+                binding.addCharacterLuckGrowth.text.toString().toInt(),
+                binding.addCharacterDefGrowth.text.toString().toInt(),
+                binding.addCharacterConGrowth.text.toString().toInt(),
+                binding.addCharacterMoveGrowth.text.toString().toInt(),
+                true)
             lifecycleScope.launch {
                 characterDao.insert(char)
             }
