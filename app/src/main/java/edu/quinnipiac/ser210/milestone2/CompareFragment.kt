6bd51f5f99911128624ebc8dfe16549d02373d37
PaulZ357 <Paul.Zegarek@quinnipiac.edu>
2024-05-03 17:45:39 -0400

    fixed compare fragment

--- app/src/main/java/edu/quinnipiac/ser210/milestone2/CompareFragment.kt
+++ app/src/main/java/edu/quinnipiac/ser210/milestone2/CompareFragment.kt
@@ -1,12 +1,18 @@
 package edu.quinnipiac.ser210.milestone2
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.core.view.get
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.lifecycleScope
+import edu.quinnipiac.ser210.milestone2.data.Character
 import edu.quinnipiac.ser210.milestone2.data.CharacterDao
 import edu.quinnipiac.ser210.milestone2.databinding.FragmentCompareBinding
 import kotlinx.coroutines.launch
@@ -14,15 +20,19 @@ import kotlinx.coroutines.launch
 class CompareFragment : Fragment() {
 
 	lateinit var binding: FragmentCompareBinding
+	private val characterIndex = MutableLiveData(0)
 	lateinit var characterDao: CharacterDao
-	override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
 		super.onCreate(savedInstanceState)
-
+		// Inflate the layout for this fragment
 		characterDao =
 			(activity?.application as DataApplication).characterDatabase.characterDao()
 		binding = FragmentCompareBinding.inflate(layoutInflater)
 
-		lifecycleScope.launch {
+		/*lifecycleScope.launch {
 			// looks for the
 			val _CHARACTER = binding.spinner.selectedItemId.toInt()
 			val character = characterDao.getCharacter(_CHARACTER)
@@ -39,13 +49,50 @@ class CompareFragment : Fragment() {
 				binding.compareCharHP.text = data.HPGrowth.toString()
 				binding.compareCharStr.text = data.strGrowth.toString()
 			}
+		}*/
+		characterIndex.observe(viewLifecycleOwner) {index: Int ->
+			characterDao.getCharacters().asLiveData()
+				.observe(viewLifecycleOwner) {
+					if (index < it.size) {
+						val character = it[index]
+						binding.currentHP.setText(character.baseHP.toString())
+						binding.currentStr.setText(character.baseStr.toString())
+						binding.currentMag.setText(character.baseMag.toString())
+						binding.currentSpeed.setText(character.baseSpd.toString())
+						binding.currentSkill.setText(character.baseSkl.toString())
+						binding.currentLuck.setText(character.baseLck.toString())
+						binding.currentDef.setText(character.baseDef.toString())
+						binding.currentCon.setText(character.baseCon.toString())
+						binding.currentMove.setText(character.baseMov.toString())
+					}
+					else {
+						characterIndex.value = characterIndex.value?.minus(1)
+					}
+				}
 		}
+
+		characterDao.getCharacters().asLiveData()
+			.observe(viewLifecycleOwner) { characters: List<Character> ->
+				binding.spinner.adapter = ArrayAdapter(
+					requireContext(),
+					R.layout.simple_spinner_item,
+					Array(characters.size) { characters[it].name }
+				)
+			}
+
+		binding.spinner.onItemSelectedListener =
+			CharacterListener(characterIndex)
+
+		return binding.root
 	}
-    override fun onCreateView(
-		inflater: LayoutInflater, container: ViewGroup?,
-		savedInstanceState: Bundle?
-	): View? {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_compare, container, false)
+	class CharacterListener(private val characterIndex: MutableLiveData<Int>) :
+		AdapterView.OnItemSelectedListener {
+		override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+			characterIndex.value = parent!!.selectedItemPosition
+		}
+
+		override fun onNothingSelected(parent: AdapterView<*>?) {
+		}
+
 	}
 }
