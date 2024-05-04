8b71c1eca0a026ed669537a202cdd1a62792c06f
Sharktroid <87317336+Sharktroid@users.noreply.github.com>
2024-05-03 22:46:58 -0400

    Can now select scroll amounts

--- app/src/main/java/edu/quinnipiac/ser210/milestone2/CompareFragment.kt
+++ app/src/main/java/edu/quinnipiac/ser210/milestone2/CompareFragment.kt
@@ -2,22 +2,23 @@ package edu.quinnipiac.ser210.milestone2
 
 import android.R
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import androidx.core.view.get
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.asLiveData
-import androidx.lifecycle.lifecycleScope
 import edu.quinnipiac.ser210.milestone2.data.Character
 import edu.quinnipiac.ser210.milestone2.data.CharacterDao
 import edu.quinnipiac.ser210.milestone2.data.Scroll
 import edu.quinnipiac.ser210.milestone2.data.ScrollDao
 import edu.quinnipiac.ser210.milestone2.databinding.FragmentCompareBinding
-import kotlinx.coroutines.launch
 
 class CompareFragment : Fragment() {
 
@@ -27,8 +28,10 @@ class CompareFragment : Fragment() {
 	lateinit var scroll: Map<Scroll, Int>
 	private lateinit var scrollDao: ScrollDao
 	private val scrollIndex = MutableLiveData(0)
+	private val scrollSums = MutableLiveData<HashMap<Scroll, Int>>(java.util.HashMap())
 	var level: Int
-    override fun onCreateView(
+
+	override fun onCreateView(
 		inflater: LayoutInflater, container: ViewGroup?,
 		savedInstanceState: Bundle?
 	): View {
@@ -38,7 +41,7 @@ class CompareFragment : Fragment() {
 			(activity?.application as DataApplication).characterDatabase.characterDao()
 		binding = FragmentCompareBinding.inflate(layoutInflater)
 
-		characterIndex.observe(viewLifecycleOwner) {index: Int ->
+		characterIndex.observe(viewLifecycleOwner) { index: Int ->
 			characterDao.getCharacters().asLiveData()
 				.observe(viewLifecycleOwner) {
 					if (index < it.size) {
@@ -61,13 +64,12 @@ class CompareFragment : Fragment() {
 						binding.currentDef.setText(character.baseDef.toString())
 						binding.currentCon.setText(character.baseCon.toString())
 						binding.currentMove.setText(character.baseMov.toString())*/
-					}
-					else {
+					} else {
 						characterIndex.value = characterIndex.value?.minus(1)
 					}
 				}
 		}
-		scrollIndex.observe(viewLifecycleOwner) {index: Int ->
+		scrollIndex.observe(viewLifecycleOwner) { index: Int ->
 			scrollDao.getScrolls().asLiveData()
 				.observe(viewLifecycleOwner) {
 					if (index < it.size) {
@@ -81,8 +83,7 @@ class CompareFragment : Fragment() {
 						binding.currentDef.setText(character.baseDef.toString())
 						binding.currentCon.setText(character.baseCon.toString())
 						binding.currentMove.setText(character.baseMov.toString())*/
-					}
-					else {
+					} else {
 						characterIndex.value = characterIndex.value?.minus(1)
 					}
 				}
@@ -100,7 +101,7 @@ class CompareFragment : Fragment() {
 			.observe(viewLifecycleOwner) { scrolls: List<Scroll> ->
 				binding.spinnerScroll.adapter = ArrayAdapter(
 					requireContext(),
-					android.R.layout.simple_spinner_item,
+					R.layout.simple_spinner_item,
 					Array(scrolls.size) { scrolls[it].name }
 				)
 			}
@@ -108,8 +109,13 @@ class CompareFragment : Fragment() {
 		binding.spinner.onItemSelectedListener =
 			CharacterListener(characterIndex)
 
+		binding.spinnerScroll.onItemSelectedListener = ScrollListener(binding, scrollSums)
+
+		binding.currentScrollCount.addTextChangedListener(CurrentScrollCountWatcher(binding.spinnerScroll, scrollSums))
+
 		return binding.root
 	}
+
 	class CharacterListener(private val characterIndex: MutableLiveData<Int>) :
 		AdapterView.OnItemSelectedListener {
 		override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
@@ -119,5 +125,37 @@ class CompareFragment : Fragment() {
 		override fun onNothingSelected(parent: AdapterView<*>?) {
 		}
 
+	}
+
+	class ScrollListener(
+		private val binding: FragmentCompareBinding,
+		private val scrollSums: LiveData<HashMap<Scroll, Int>>
+	) :
+		AdapterView.OnItemSelectedListener {
+		override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+			binding.currentScrollCount.setText(
+				scrollSums.value!![parent!!.selectedItem]?.toString()
+					?: ""
+			)
+		}
+
+		override fun onNothingSelected(parent: AdapterView<*>?) {
+		}
+
+	}
+
+	class CurrentScrollCountWatcher(
+		private val spinnerScroll: Spinner,
+		private val scrollSums: MutableLiveData<HashMap<Scroll, Int>>
+	) : TextWatcher {
+		override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+		}
+
+		override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+			scrollSums.value!![spinnerScroll.selectedItem as Scroll] = s.toString().toInt()
+		}
+
+		override fun afterTextChanged(s: Editable?) {
+		}
 	}
 }
